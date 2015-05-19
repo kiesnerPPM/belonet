4ce7d714bd3b34c2bc9c710dc422fadb1ee1dbdd
Stefan Herrnleben <s.herrnleben@syslex.de>
2015-05-19 22:57:04 +0200

    added function to delete worker

--- Packages/Application/Belonet.Ssn/Classes/Belonet/Ssn/Controller/WorkerController.php
+++ Packages/Application/Belonet.Ssn/Classes/Belonet/Ssn/Controller/WorkerController.php
@@ -10,64 +10,80 @@ use TYPO3\Flow\Annotations as Flow;
 
 class WorkerController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 
+  /**
+   * @Flow\Inject
+   * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
+   */
+  protected $persistenceManager;
+
+  /**
+   * @Flow\Inject
+   * @var \Belonet\Ssn\Domain\Repository\WorkerRepository
+   */
+  protected $workerRepository;
+
+
+  /**
+   * @param \Belonet\Ssn\Domain\Model\Worker $worker
+   * @return void
+   */
+  public function showWorkerDataAction() {
+    $allWorkers = $this->workerRepository->findAll()->toArray();
+    $this->persistenceManager->persistAll();
+    $this->view->assign("allWorkers", $allWorkers);
+  }
+
+  /**
+   * @param \Belonet\Ssn\Domain\Model\Worker $worker
+   * @return void
+   */
+  public function confirmWorkerDeletionAction(\Belonet\Ssn\Domain\Model\Worker $worker) {
+    $this->view->assign("worker", $worker);
+  }
+
+  /**
+   * @param \Belonet\Ssn\Domain\Model\Worker $worker
+   * @return void
+   */
+  public function deleteWorkerAction(\Belonet\Ssn\Domain\Model\Worker $worker) {
+    $this->workerRepository->remove($worker);
+    $this->persistenceManager->persistAll();
+    $this->redirect('showWorkerData');
+  }
+
+  /**
+   * Redirection to showWorkerData
+   * @return void
+   */
+  /**   public function redirectionAction() {
+   *       $this->redirect("showWorkerData", null, null, array("worker"=> $worker));
+   *    }
+   */
+
+
+  /**
+   *
+   */
+  public function viewWorkerCreationAction() {
+
+  }
+
+  /**
+   * @param \Belonet\Ssn\Domain\Model\Worker $worker
+   */
+  public function createNewWorkerAction(\Belonet\Ssn\Domain\Model\Worker $worker) {
     /**
-     * @Flow\Inject
-     * @var \TYPO3\Flow\Persistence\PersistenceManagerInterface
+     * 1. Version, worker wird statisch angelegt
+     *$worker = new \Belonet\Ssn\Domain\Model\Worker();
+     *$worker->setWorkerId("abcd");
+     *$this->workerRepository->add($worker);
+     *$this->persistenceManager->persistAll();
+     *$this->redirect("showWorkerData", null, null, array("worker"=> $worker));
      */
-    protected $persistenceManager;
-
-    /**
-     * @Flow\Inject
-     * @var \Belonet\Ssn\Domain\Repository\WorkerRepository
-     */
-    protected $workerRepository;
-
-
-
-
-	/**
- * @param \Belonet\Ssn\Domain\Model\Worker $worker
- * @return void
- */
-    public function showWorkerDataAction() {
-        $allWorkers = $this->workerRepository->findAll()->toArray();
-        $this->persistenceManager->persistAll();
-        $this->view->assign("allWorkers",$allWorkers);
-    }
-
-    /**
-     * Redirection to showWorkerData
-     * @return void
-     */
- /**   public function redirectionAction() {
- *       $this->redirect("showWorkerData", null, null, array("worker"=> $worker));
-*    }
-*/
-
-
-    /**
-     *
-     */
-    public function viewWorkerCreationAction(){
-
-    }
-
-    /**
-     * @param \Belonet\Ssn\Domain\Model\Worker $worker
-     */
-    public function createNewWorkerAction(\Belonet\Ssn\Domain\Model\Worker $worker){
-        /**
-         * 1. Version, worker wird statisch angelegt
-        *$worker = new \Belonet\Ssn\Domain\Model\Worker();
-        *$worker->setWorkerId("abcd");
-        *$this->workerRepository->add($worker);
-        *$this->persistenceManager->persistAll();
-        *$this->redirect("showWorkerData", null, null, array("worker"=> $worker));
-         */
-        //2. Version: worker wird durch Form angelegt
-        $this->workerRepository->add($worker);
-        $this->redirect("showWorkerData", null, null, array());
-    }
+    //2. Version: worker wird durch Form angelegt
+    $this->workerRepository->add($worker);
+    $this->redirect("showWorkerData", null, null, array());
+  }
 
 
 }
